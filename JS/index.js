de81d70dbc216e92fb3dc4649fb952e2402bbc02
Célestin ROUX <celestin.roux@etu.univ-nantes.fr>
2025-01-22 11:05:33 +0100

    ajout icons + animation image centrale

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
   const image = document.querySelector(".img-centrale");
+  const overlayImage = document.querySelector("#overlay-centrale");
   const divs = document.querySelectorAll(".hidden-div");
 
   let isVisible = false;
   let isAnimating = false;
 
-  image.addEventListener("click", function() {
+  overlayImage.addEventListener("click", function() {
     if (isAnimating) return;
 
     isVisible = !isVisible;
@@ -18,8 +19,10 @@ document.addEventListener("DOMContentLoaded", function() {
           if (index === divs.length - 1) {
             isAnimating = false; // Fin de l'animation
           }
-        }, index * 200);
-      });
+        }, index * 200);        
+      });      
+      image.classList.remove("animation-breathing");
+      overlayImage.classList.remove("animation-breathing");
     } else {
       [...divs].reverse().forEach((div, index) => {
         setTimeout(() => {
@@ -27,8 +30,10 @@ document.addEventListener("DOMContentLoaded", function() {
           if (index === divs.length - 1) {
             isAnimating = false; // Fin de l'animation
           }
-        }, index * 200);
-      });
+        }, index * 200);        
+      });      
+      image.classList.add("animation-breathing");
+      overlayImage.classList.add("animation-breathing");
     }
   });
 });
